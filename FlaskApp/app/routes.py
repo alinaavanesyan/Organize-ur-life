81407c1a78e0fddb26ec97ab036c4ecc61bb21df
Ksenia <90270145+XenyaBob@users.noreply.github.com>
2022-05-09 15:15:23 +0300

    Update routes.py

--- FlaskApp/app/routes.py
+++ FlaskApp/app/routes.py
@@ -307,3 +307,21 @@ def image1():
 def image2():
     path_image2 = os.path.join(app.config['UPLOAD_FOLDER'], 'кот2.png')
     return send_file(path_image2)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=int(id)).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    quer = Todo.query
+    filtr = quer.filter_by(id=int(id))
+    form_name = "todo_" + str(id)
+    todo_text = request.form.get(form_name, "")
+    filtr.update({"text": todo_text})
+    db.session.commit()
+    return redirect(url_for('home'))
